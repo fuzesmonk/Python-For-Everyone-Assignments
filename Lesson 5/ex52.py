c21e3b88a07196805616d50ce895f5aa2ac303e3
fuzesmonk <55888489+fuzesmonk@users.noreply.github.com>
2022-08-01 22:37:30 -0700

    fuck

--- Lesson 5/ex52.py
+++ Lesson 5/ex52.py
@@ -4,26 +4,25 @@
 #  Enter 7, 2, bob, 10, and 4 and match the output below. 
 
 done = True
+first_loop = True
+largest_number = None
+smallest_number = None
+
 while(done) :
-    first_loop = True
-    answer = input("Input Number: ")
-    if first_loop == True :
-        largest_number = answer
-        smallest_number = answer
-    else :
-        try
+    answer = int(input("Input Number: "))
+    try:
+        print("Not a valid Answer")
+    except answer == type(int) :
         if answer > largest_number :
             largest_number = answer
         elif answer < smallest_number :
             smallest_number = answer
-        else:
-            print("Not a valid answer")
-        if "done"  :
-            if answer == int:
-                break
-
-print(largest_number)
-print(smallest_number)
+        else :
+            continue    
+    if answer == str("done") :
+        print(f'The largest number is {largest_number}')
+        print(f'The smallest number is {smallest_number}')
+        break
                 
 
 
